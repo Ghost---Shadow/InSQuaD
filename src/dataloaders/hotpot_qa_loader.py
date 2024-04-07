0441cd915dcb8ffda522097ef5f697a0b49e554e
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-07 17:38:16 -0500

    Hotpotqa loader test

---
+++ src/dataloaders/hotpot_qa_loader.py
@@ -0,0 +1,68 @@
+from datasets import load_dataset
+from dataloaders.base import BaseDataset
+
+
+class HotpotQaDataset(BaseDataset):
+    NAME = "hotpot_qa"
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.dataset = load_dataset("hotpot_qa", "distractor")
+
+    @staticmethod
+    def collate_fn(batch):
+        batch_questions = []
+        batch_flat_sentences = []
+        batch_relevant_sentence_indexes = []
+        batch_labels_mask = []
+        # batch_flag_for_error = []
+
+        for item in batch:
+            # https://github.com/hotpotqa/hotpot/issues/47
+            # flag_for_error = False
+            question = item["question"]
+            flat_sentences = []
+
+            index_lut = {}
+            for title, sentences in zip(
+                item["context"]["title"], item["context"]["sentences"]
+            ):
+                sent_counter = 0
+
+                for sentence in sentences:
+                    index_lut[(title, sent_counter)] = len(flat_sentences)
+                    flat_sentences.append(sentence)
+                    sent_counter += 1
+
+            relevant_sentence_indexes = []
+            for title, sent_id in zip(
+                item["supporting_facts"]["title"], item["supporting_facts"]["sent_id"]
+            ):
+                key = (title, sent_id)
+                if key in index_lut:
+                    flat_index = index_lut[key]
+                    relevant_sentence_indexes.append(flat_index)
+                # else:
+                #     flag_for_error = True
+
+            # Sort if necessary
+            relevant_sentence_indexes = sorted(relevant_sentence_indexes)
+
+            labels_mask = [False] * len(flat_sentences)
+            for index in relevant_sentence_indexes:
+                labels_mask[index] = True
+
+            batch_questions.append(question)
+            batch_flat_sentences.append(flat_sentences)
+            batch_relevant_sentence_indexes.append(relevant_sentence_indexes)
+            batch_labels_mask.append(labels_mask)
+            # batch_flag_for_error.append(flag_for_error)
+
+        return {
+            "question": batch_questions,
+            "documents": batch_flat_sentences,
+            "relevant_indexes": batch_relevant_sentence_indexes,
+            "correct_mask": batch_labels_mask,
+            "paraphrase_masks": [],  # No paraphrase for base dataset
+            # "flag_for_error": batch_flag_for_error,
+        }
---
+++ src/dataloaders/hotpot_qa_loader_test.py
@@ -0,0 +1,69 @@
+import unittest
+from dataloaders.hotpot_qa_loader import HotpotQaDataset
+from tqdm import tqdm
+from config import Config
+from train_utils import set_seed
+import numpy as np
+
+
+# python -m unittest dataloaders.hotpot_qa_loader_test.TestHotpotQaLoader -v
+class TestHotpotQaLoader(unittest.TestCase):
+    # python -m unittest dataloaders.hotpot_qa_loader_test.TestHotpotQaLoader.test_hotpot_qa_loader -v
+    def test_hotpot_qa_loader(self):
+        # Set seed for deterministic testing
+        set_seed(42)
+
+        config = Config.from_file("experiments/dummy_experiment.yaml")
+        config.training.batch_size = 1
+
+        dataset = HotpotQaDataset(config)
+        train_loader = dataset.get_loader(split="train")
+
+        batch = next(iter(train_loader))
+        expected = "The  Atlantic Islands of Galicia National Park and the Timanfaya National Park are the properties of what country?"
+        actual = batch["question"][0]
+        assert actual == expected, actual
+
+        expected = [
+            'The Atlantic Islands of Galicia National Park (Galician: "Parque Nacional das Illas Atlánticas de Galicia" , Spanish: "Parque Nacional de las Islas Atlánticas de Galicia" ) is the only national park located in the autonomous community of Galicia, Spain.',
+            'Timanfaya National Park (Spanish: "Parque Nacional de Timanfaya" ) is a Spanish national park in the southwestern part of the island of Lanzarote, Canary Islands.',
+        ]
+        actual = list(np.array(batch["documents"][0])[batch["relevant_indexes"][0]])
+        assert expected == actual, actual
+        actual = list(np.array(batch["documents"][0])[batch["correct_mask"][0]])
+        assert expected == actual, actual
+
+        # Validation loader
+        val_loader = dataset.get_loader(split="validation")
+        batch = next(iter(val_loader))
+        expected = "Were Scott Derrickson and Ed Wood of the same nationality?"
+        actual = batch["question"][0]
+        assert actual == expected, actual
+
+        expected = [
+            "Scott Derrickson (born July 16, 1966) is an American director, screenwriter and producer.",
+            "Edward Davis Wood Jr. (October 10, 1924 – December 10, 1978) was an American filmmaker, actor, writer, producer, and director.",
+        ]
+        actual = list(np.array(batch["documents"][0])[batch["relevant_indexes"][0]])
+        assert expected == actual, actual
+        assert len(batch["documents"][0]) == len(batch["correct_mask"][0])
+        actual = list(np.array(batch["documents"][0])[batch["correct_mask"][0]])
+        assert expected == actual, actual
+
+    # python -m unittest dataloaders.hotpot_qa_loader_test.TestHotpotQaLoader.test_no_bad_rows -v
+    def test_no_bad_rows(self):
+        # https://github.com/hotpotqa/hotpot/issues/47
+        config = Config.from_file("experiments/dummy_experiment.yaml")
+        config.training.batch_size = 2
+        dataset = HotpotQaDataset(config)
+        train_loader = dataset.get_loader(split="train")
+        val_loader = dataset.get_loader(split="validation")
+        for _ in tqdm(train_loader):
+            ...
+
+        for _ in tqdm(val_loader):
+            ...
+
+
+if __name__ == "__main__":
+    unittest.main()
