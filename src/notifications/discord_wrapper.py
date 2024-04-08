04ee91cc1c67fd91c92e94aa4b21c32876c6c9a2
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-08 10:09:26 -0500

    Discord notifications

---
+++ src/notifications/discord_wrapper.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+import requests
+import json
+
+load_dotenv()
+
+
+def send_discord_notification(message):
+    try:
+        data = {"content": message}
+        headers = {"Content-Type": "application/json"}
+        webhook_url = os.environ.get("DISCORD_API_HOOK")
+
+        # POST request to the webhook URL
+        response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
+
+        # Check for success
+        if response.status_code == 204:
+            print("Notification sent successfully.")
+        else:
+            print("Failed to send notification.")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    message = ":eggplant: Experiment completed!"
+    send_discord_notification(message)
