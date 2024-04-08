89ca4d844682ecf71ee57d1403b58b36437b03b3
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-07 19:16:21 -0500

    Wire up datasets

--- src/dataloaders/__init__.py
+++ src/dataloaders/__init__.py
@@ -1,9 +1,18 @@
 from dataloaders.dummy import DummyDataset
 from dataloaders.mrpc import MRPC
+from dataloaders.dummy_hotpot_qa_with_q_loader import DummyHotpotQaWithQDataset
+from dataloaders.hotpot_qa_loader import HotpotQaDataset
+from dataloaders.hotpot_qa_with_q_loader import HotpotQaWithQDataset
+from dataloaders.wiki_multihop_qa_loader import WikiMultihopQaDataset
+from dataloaders.wiki_multihop_qa_with_q_loader import WikiMultihopQaWithQDataset
 
 
 DATALOADERS_LUT = {
     MRPC.NAME: MRPC,
     DummyDataset.NAME: DummyDataset,
-    "hotpotqawithq": DummyDataset,  # TODO
+    HotpotQaDataset.NAME: HotpotQaDataset,
+    HotpotQaWithQDataset.NAME: HotpotQaWithQDataset,
+    DummyHotpotQaWithQDataset.NAME: DummyHotpotQaWithQDataset,
+    WikiMultihopQaDataset.NAME: WikiMultihopQaDataset,
+    WikiMultihopQaWithQDataset.NAME: WikiMultihopQaWithQDataset,
 }
