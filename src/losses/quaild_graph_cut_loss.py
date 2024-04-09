3f9c4e62f3561455c8e412eee1ba919f735f2224
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-09 12:19:49 -0500

    Fix graph cut loss

--- src/losses/quaild_graph_cut_loss.py
+++ src/losses/quaild_graph_cut_loss.py
@@ -9,17 +9,27 @@ class QuaildGraphCutLoss(nn.Module):
         self.epsilon = 0.0  # Adjust if necessary
 
     def forward(self, a, b):
-        # Ensure a and b are 2D [batch_size, features]
-        b_t = b.transpose(0, 1)  # Now [features, batch_size]
+        if a.shape == 2:
+            a = a.unsqueeze(0)
+        if b.shape == 2:
+            b = b.unsqueeze(0)
+        assert len(a.shape) == 3
+        assert len(b.shape) == 3
+        batch_size, num_docs, features = a.shape
 
-        # Matrix multiplication [batch_size, batch_size]
+        # Ensure a and b are 2D [batch_size, num_docs, features]
+        b_t = b.transpose(1, 2)  # Now [batch_size, features, num_docs]
+
+        # Matrix multiplication [batch_size, num_docs, num_docs]
         similarity = torch.matmul(a, b_t)
 
-        # Adjust the computation here if you intend to have a different form of aggregation
-        # Normalize by number of elements in b
-        loss = 2 * self.lambd * similarity.sum(dim=-1)
+        # Normalize and rescale
+        similarity = similarity.reshape([batch_size, num_docs * num_docs])
+        aggregated_similarity = 2 * self.lambd * similarity.mean(dim=-1)
 
-        # Adjust the lower bound for each item in the batch, if necessary
+        loss = -aggregated_similarity
+
+        # Adjust the lower bound for each item in the batch
         theoretical_lower_bound = 2 * self.lambd * -1
         loss = loss - theoretical_lower_bound + self.epsilon
 
--- src/losses/quaild_graph_cut_loss_test.py
+++ src/losses/quaild_graph_cut_loss_test.py
@@ -9,59 +9,50 @@ from train_utils import set_seed
 # python -m unittest losses.quaild_graph_cut_loss_test.TestQuaildGraphCut -v
 class TestQuaildGraphCut(unittest.TestCase):
     def setUp(self):
-        # Setup code runs before each test method
-        self.config = Config.from_file("experiments/quaild_test_experiment.yaml")
-
-        self.model = QuaildGraphCutLoss(self.config)
-        self.a = torch.rand((5, 3))  # Example tensor a
-        self.b = torch.rand((5, 3))  # Example tensor b
-
-    # python -m unittest losses.quaild_graph_cut_loss_test.TestQuaildGraphCut.test_forward_output_shape -v
-    def test_forward_output_shape(self):
-        """Test if the output of the forward method has the correct shape."""
-        loss = self.model(self.a, self.b)
-        self.assertIsInstance(loss, torch.Tensor)  # Check if output is a torch tensor
-        self.assertEqual(loss.shape, torch.Size([]))  # Check if output is a scalar
-
-    # python -m unittest losses.quaild_graph_cut_loss_test.TestQuaildGraphCut.test_forward_positive_loss -v
-    def test_forward_positive_loss(self):
-        """Test if the loss is positive."""
-        loss = self.model(self.a, self.b)
-        self.assertGreater(loss.item(), 0)  # Check if loss is greater than 0
+        config = Config.from_file("experiments/quaild_test_experiment.yaml")
+        self.loss_fn = QuaildGraphCutLoss(config)
 
     # python -m unittest losses.quaild_graph_cut_loss_test.TestQuaildGraphCut.test_theoretical_lower_bound -v
     def test_theoretical_lower_bound(self):
-        a = torch.tensor([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-        b = torch.tensor([[-1.0, 0.0, 0.0], [0.0, -1.0, 0.0], [0.0, 0.0, -1.0]])
-        loss = self.model(a, b)
+        a = torch.tensor(
+            [[[1.0, 0.0, 0.0], [1.0, 0.0, 0.0]], [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]]
+        )
+        b = torch.tensor(
+            [[[1.0, 0.0, 0.0], [1.0, 0.0, 0.0]], [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]]
+        )
+        loss = self.loss_fn(a, b)
         self.assertAlmostEqual(loss.item(), 0.0)
-        print(loss)
 
-    # python -m unittest losses.quaild_graph_cut_loss_test.TestQuaildGraphCut.test_lambd_effect -v
-    def test_lambd_effect(self):
-        """Test if changing lambd affects the loss."""
-        initial_loss = self.model(self.a, self.b)
-        # Change lambda value and re-test
-        self.config.training.loss.lambd = 1.0  # Change lambda to a different value
-        # Reinitialize the model with the new lambda
-        self.model = QuaildGraphCutLoss(self.config)
-        new_loss = self.model(self.a, self.b)
-        # Check if loss values are different
-        self.assertNotEqual(initial_loss.item(), new_loss.item())
+    # python -m unittest losses.quaild_graph_cut_loss_test.TestQuaildGraphCut.test_theoretical_upper_bound -v
+    def test_theoretical_upper_bound(self):
+        a = torch.tensor(
+            [[[1.0, 0.0, 0.0], [1.0, 0.0, 0.0]], [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]]
+        )
+        b = torch.tensor(
+            [[[-1.0, 0.0, 0.0], [-1.0, 0.0, 0.0]], [[0.0, 0.0, -1.0], [0.0, 0.0, -1.0]]]
+        )
+        loss = self.loss_fn(a, b)
+        self.assertAlmostEqual(loss.item(), 2.0)
 
     # python -m unittest losses.quaild_graph_cut_loss_test.TestQuaildGraphCut.test_overfit -v
     def test_overfit(self):
         set_seed(42)
 
         # embedding_size = 256
-        # batch_size = 10
+        # num_docs = 10
+        # batch_size = 2
 
         embedding_size = 3
-        batch_size = 10
+        num_docs = 4
+        batch_size = 2
 
         # Create random tensors for a and b
-        original_a = torch.randn(batch_size, embedding_size, requires_grad=False)
-        original_b = torch.randn(batch_size, embedding_size, requires_grad=False)
+        original_a = torch.randn(
+            batch_size, num_docs, embedding_size, requires_grad=False
+        )
+        original_b = torch.randn(
+            batch_size, num_docs, embedding_size, requires_grad=False
+        )
 
         # Normalize a and b
         normalized_a = original_a / torch.norm(original_a)
@@ -73,13 +64,14 @@ class TestQuaildGraphCut(unittest.TestCase):
 
         config = Config.from_file("experiments/quaild_test_experiment.yaml")
         loss_fn = QuaildGraphCutLoss(config)
-        optimizer = AdamW([a, b], lr=0.01)
+        optimizer = AdamW([a, b], lr=0.1)
 
         # Training loop
         for epoch in range(100):
             optimizer.zero_grad()
             loss = loss_fn(a, b)
-            loss = loss.mean()
+
+            mse_loss = torch.nn.functional.mse_loss(a, b, reduction="mean")
 
             loss.backward()
             optimizer.step()
@@ -89,14 +81,13 @@ class TestQuaildGraphCut(unittest.TestCase):
                 a /= torch.norm(a)
                 b /= torch.norm(b)
 
-            cosine = torch.mean(torch.matmul(a, b.transpose(0, 1)))
-            print(
-                f"Epoch {epoch+1}, Loss: {loss.item()}, Cosine: {cosine.item()}",
-                # a.tolist(),
-                # b.tolist(),
-            )
+            # print(
+            #     f"Epoch {epoch+1}, Loss: {loss.item()}, MSE: {mse_loss.item()}",
+            #     # a.tolist(),
+            #     # b.tolist(),
+            # )
 
-        assert cosine.item() <= 0.0, cosine.item()
+        assert mse_loss.item() <= 1e-4, mse_loss.item()
 
 
 if __name__ == "__main__":
