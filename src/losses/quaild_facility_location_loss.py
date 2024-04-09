019e8e98c1fc228a2ba275e9a12aeee9b0759912
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-09 12:38:53 -0500

    Fix facility location overfit test

--- src/losses/quaild_facility_location_loss.py
+++ src/losses/quaild_facility_location_loss.py
@@ -12,30 +12,46 @@ class QuaildFacilityLocationLoss(nn.Module):
         """
         super(QuaildFacilityLocationLoss, self).__init__()
         self.lambd = config.training.loss.lambd
+        self.epsilon = 0.0  # Adjust if needed
 
     def forward(self, a, b):
         """
         Compute the Facility Location loss between two sets of embeddings.
 
         Parameters:
-        - a (Tensor): The embeddings tensor of shape (batch_size, embedding_size).
-        - b (Tensor): The embeddings tensor of shape (batch_size, embedding_size),
+        - a (Tensor): The embeddings tensor of shape (batch_size, num_docs, embedding_size).
+        - b (Tensor): The embeddings tensor of shape (batch_size, num_docs, embedding_size),
                       representing a different set or the same set as 'a'.
 
         Returns:
         - loss (Tensor): The computed Facility Location loss.
         """
+        if a.shape == 2:
+            a = a.unsqueeze(0)
+        if b.shape == 2:
+            b = b.unsqueeze(0)
+        assert len(a.shape) == 3
+        assert len(b.shape) == 3
+
         # Compute similarity matrix S using dot product
         # S_ij = a_i . b_j^T
-        similarities = torch.matmul(a, b.transpose(0, 1))
+        # [batch_size, num_docs, features] to [batch_size, num_docs, num_docs]
+        similarities = torch.matmul(a, b.transpose(1, 2))
 
         # Compute max similarity for each i in a with all j in b
+        # [batch_size, num_docs, num_docs] to [batch_size, num_docs]
         max_similarities_a_to_b, _ = torch.max(similarities, dim=1)
-        max_similarities_b_to_a, _ = torch.max(similarities, dim=0)
+        max_similarities_b_to_a, _ = torch.max(similarities, dim=2)
 
         # Sum these max similarities and apply the regularization term
-        loss = (
-            max_similarities_a_to_b.sum() + self.lambd * max_similarities_b_to_a.sum()
-        )
+        mean_max_similarities = max_similarities_a_to_b.mean(
+            dim=-1
+        ) + self.lambd * max_similarities_b_to_a.mean(dim=-1)
 
-        return loss
+        loss = -mean_max_similarities
+
+        # Adjust the lower bound for each item in the batch
+        theoretical_lower_bound = -1 + self.lambd * -1
+        loss = loss - theoretical_lower_bound + self.epsilon
+
+        return loss.mean()
--- src/losses/quaild_facility_location_loss_test.py
+++ src/losses/quaild_facility_location_loss_test.py
@@ -10,53 +10,52 @@ from train_utils import set_seed
 # python -m unittest losses.quaild_facility_location_loss_test.TestQuaildFacilityLocationLoss -v
 class TestQuaildFacilityLocationLoss(unittest.TestCase):
     def setUp(self):
-        self.embedding_size = 4
-        self.batch_size = 3
-        self.a = torch.tensor(
-            [[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]]
-        )
-        self.b = torch.tensor(
-            [[0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0]]
-        )
-
-    # python -m unittest losses.quaild_facility_location_loss_test.TestQuaildFacilityLocationLoss.test_loss_value_known_inputs -v
-    def test_loss_value_known_inputs(self):
-        """Test the loss computation with known inputs."""
-        lambd = 1.0
         config = Config.from_file("experiments/quaild_test_experiment.yaml")
-        config.training.loss.lambd = lambd
+        config.training.loss.lambd = 1.0
         config.training.loss.type = "facility_location"
-        loss_module = QuaildFacilityLocationLoss(config)
-        loss = loss_module(self.a, self.b)
-        expected_loss_value = 3.0 + 3.0 * lambd
-        self.assertAlmostEqual(loss.item(), expected_loss_value, places=4)
+        self.loss_fn = QuaildFacilityLocationLoss(config)
 
-    # python -m unittest losses.quaild_facility_location_loss_test.TestQuaildFacilityLocationLoss.test_lambd_impact -v
-    def test_lambd_impact(self):
-        """Test the impact of lambda on the loss value."""
-        config = Config.from_file("experiments/quaild_test_experiment.yaml")
+    # python -m unittest losses.quaild_facility_location_loss_test.TestQuaildFacilityLocationLoss.test_theoretical_lower_bound -v
+    def test_theoretical_lower_bound(self):
+        a = torch.tensor(
+            [[[1.0, 0.0, 0.0], [0.0, 0.0, 1.0]], [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]]
+        )
+        b = torch.tensor(
+            [[[0.0, 0.0, 1.0], [1.0, 0.0, 0.0]], [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]]
+        )
+        loss = self.loss_fn(a, b)
+        self.assertAlmostEqual(loss.item(), 0.0)
 
-        lambd_values = [0.5, 1.0, 1.5]
-        previous_loss = None
-        for lambd in lambd_values:
-            config.training.loss.lambd = lambd
-            loss_module = QuaildFacilityLocationLoss(config)
-            loss = loss_module(self.a, self.b)
-            if previous_loss is not None:
-                # Ensure that increasing lambda increases the loss value.
-                self.assertGreater(loss.item(), previous_loss.item())
-            previous_loss = loss
+    # python -m unittest losses.quaild_facility_location_loss_test.TestQuaildFacilityLocationLoss.test_theoretical_upper_bound -v
+    def test_theoretical_upper_bound(self):
+        a = torch.tensor(
+            [[[1.0, 0.0, 0.0], [1.0, 0.0, 0.0]], [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]]
+        )
+        b = torch.tensor(
+            [[[-1.0, 0.0, 0.0], [-1.0, 0.0, 0.0]], [[0.0, 0.0, -1.0], [0.0, 0.0, -1.0]]]
+        )
+        loss = self.loss_fn(a, b)
+        self.assertAlmostEqual(loss.item(), 4.0)
 
     # python -m unittest losses.quaild_facility_location_loss_test.TestQuaildFacilityLocationLoss.test_overfit -v
     def test_overfit(self):
         set_seed(42)
 
+        # embedding_size = 256
+        # num_docs = 10
+        # batch_size = 2
+
         embedding_size = 3
-        batch_size = 1
+        num_docs = 4
+        batch_size = 2
 
         # Create random tensors for a and b
-        original_a = torch.randn(batch_size, embedding_size, requires_grad=False)
-        original_b = torch.randn(batch_size, embedding_size, requires_grad=False)
+        original_a = torch.randn(
+            batch_size, num_docs, embedding_size, requires_grad=False
+        )
+        original_b = torch.randn(
+            batch_size, num_docs, embedding_size, requires_grad=False
+        )
 
         # Normalize a and b
         normalized_a = original_a / torch.norm(original_a)
@@ -66,14 +65,15 @@ class TestQuaildFacilityLocationLoss(unittest.TestCase):
         a = torch.tensor(normalized_a, requires_grad=True)
         b = torch.tensor(normalized_b, requires_grad=True)
 
-        config = Config.from_file("experiments/quaild_test_experiment.yaml")
-        loss_fn = QuaildFacilityLocationLoss(config)
-        optimizer = AdamW([a, b], lr=0.01)
+        optimizer = AdamW([a, b], lr=0.1)
 
         # Training loop
         for epoch in range(100):
             optimizer.zero_grad()
-            loss = loss_fn(a, b)
+            loss = self.loss_fn(a, b)
+
+            mse_loss = torch.nn.functional.mse_loss(a, b, reduction="mean")
+
             loss.backward()
             optimizer.step()
 
@@ -82,14 +82,13 @@ class TestQuaildFacilityLocationLoss(unittest.TestCase):
                 a /= torch.norm(a)
                 b /= torch.norm(b)
 
-            cosine = torch.sum(torch.matmul(a, b.transpose(0, 1)))
             # print(
-            #     f"Epoch {epoch+1}, Loss: {loss.item()}, Cosine: {cosine.item()}",
-            #     a.tolist(),
-            #     b.tolist(),
+            #     f"Epoch {epoch+1}, Loss: {loss.item()}, MSE: {mse_loss.item()}",
+            #     # a.tolist(),
+            #     # b.tolist(),
             # )
 
-        assert cosine.item() < -0.9, cosine.item()
+        assert mse_loss.item() <= 1e-1, mse_loss.item()
 
 
 if __name__ == "__main__":
