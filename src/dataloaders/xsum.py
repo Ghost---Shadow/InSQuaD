a5e2ec3689649dcb0ff94fe5932add0fd2bde59a
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-15 11:02:19 -0500

    xsum dataloader

---
+++ src/dataloaders/xsum.py
@@ -0,0 +1,27 @@
+from dataloaders.base import BaseDataset
+
+
+class XsumDataset(BaseDataset):
+    LABELS = {}
+
+    NAME = "xsum"
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.cached_load_dataset(XsumDataset.NAME, ("EdinburghNLP/xsum",))
+
+    @staticmethod
+    def collate_fn(batch):
+        prompts = []
+        labels = []
+        for item in batch:
+            document, summary = (
+                item["document"],
+                item["summary"],
+            )
+            document = document.replace("\n", "")
+            summary = summary.replace("\n", "")
+            prompt = f"Document: {document}\nSummary:"
+            prompts.append(prompt)
+            labels.append(summary)
+        return {"prompts": prompts, "labels": labels}
