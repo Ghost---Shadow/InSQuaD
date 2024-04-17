7a7ac601640c1acb57edfcac88fa90f65ec99d7e
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-17 09:55:36 -0500

    Random pipeline end to end

--- src/offline_eval_pipeline_test.py
+++ src/offline_eval_pipeline_test.py
@@ -44,6 +44,20 @@ class TestOfflineEvaluationPipeline(unittest.TestCase):
         pipeline.generate_few_shots()
         pipeline.run_inference(skip_if_done=False)
 
+    # python -m unittest offline_eval_pipeline_test.TestOfflineEvaluationPipeline.test_random_flow -v
+    def test_random_flow(self):
+        config_path = "experiments/random_test_experiment.yaml"
+
+        config = Config.from_file(config_path)
+        pipeline = OfflineEvaluationPipeline(config)
+        pipeline.set_seed(42)
+        pipeline.current_dataset_name = "mrpc"
+
+        # Should not crash
+        pipeline.shortlist(skip_if_done=True)
+        pipeline.generate_few_shots(skip_if_done=True)
+        pipeline.run_inference(skip_if_done=True)
+
 
 if __name__ == "__main__":
     unittest.main()
