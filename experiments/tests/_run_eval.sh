2110225d96105d7d276d34a6c97ebe0537cb1b9d
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-18 01:37:13 -0500

    Update script names

--- experiments/tests/_run_eval.sh
+++ experiments/tests/_run_eval.sh
@@ -1,8 +1,8 @@
-python src/offline_eval.py --config=experiments/tests/quaild_gc_mpnet_neo175.yaml
 python src/offline_eval.py --config=experiments/tests/dummy_experiment.yaml
-python src/offline_eval.py --config=experiments/tests/random_test_experiment.yaml
-python src/offline_eval.py --config=experiments/tests/quaild_test_experiment.yaml
 python src/offline_eval.py --config=experiments/tests/quaild_gc_mpnet_gpt2.yaml
+python src/offline_eval.py --config=experiments/tests/quaild_gc_mpnet_neo175.yaml
+python src/offline_eval.py --config=experiments/tests/quaild_test_experiment.yaml
+python src/offline_eval.py --config=experiments/tests/random_test_experiment.yaml
 python src/offline_eval.py --config=experiments/tests/zeroshot_test_experiment.yaml
 
 source devops/stop_current_gcp_instance.sh
