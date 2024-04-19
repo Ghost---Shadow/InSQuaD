45d368744a201532d5bf0bebeeefb0b7f3758eb8
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-19 18:47:38 -0500

    New experiment scripts

--- experiments/tests/_run_eval.sh
+++ experiments/tests/_run_eval.sh
@@ -1,9 +1,11 @@
 python src/offline_eval.py --config=experiments/tests/fastvotek_test_experiment.yaml
+python src/offline_eval.py --config=experiments/tests/ideal_test_experiment.yaml
 python src/offline_eval.py --config=experiments/tests/leastconfidence_test_experiment.yaml
 python src/offline_eval.py --config=experiments/tests/quaild_gc_mpnet_gpt2.yaml
 python src/offline_eval.py --config=experiments/tests/quaild_gc_mpnet_neo175.yaml
 python src/offline_eval.py --config=experiments/tests/quaild_test_experiment.yaml
 python src/offline_eval.py --config=experiments/tests/random_test_experiment.yaml
+python src/offline_eval.py --config=experiments/tests/shortlistandtopk_test_experiment.yaml
 python src/offline_eval.py --config=experiments/tests/votek_test_experiment.yaml
 python src/offline_eval.py --config=experiments/tests/zeroshot_test_experiment.yaml
 
