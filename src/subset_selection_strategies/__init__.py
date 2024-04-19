09142ee2108ffeca28d69810eed31b63df8062db
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-19 10:03:20 -0500

    fast vote k rename

--- src/subset_selection_strategies/__init__.py
+++ src/subset_selection_strategies/__init__.py
@@ -1,4 +1,4 @@
-from subset_selection_strategies.fast_vote_k import FastVoteKStrategy
+from subset_selection_strategies.fast_vote_k_subset import FastVoteKSubsetStrategy
 from subset_selection_strategies.quaild_gain_cutoff import QuaildGainCutoffStrategy
 from subset_selection_strategies.top_k_strategy import TopKStrategy
 
@@ -6,5 +6,5 @@ from subset_selection_strategies.top_k_strategy import TopKStrategy
 SUBSET_SELECTION_STRATEGIES_LUT = {
     QuaildGainCutoffStrategy.NAME: QuaildGainCutoffStrategy,
     TopKStrategy.NAME: TopKStrategy,
-    FastVoteKStrategy.NAME: FastVoteKStrategy,
+    FastVoteKSubsetStrategy.NAME: FastVoteKSubsetStrategy,
 }
