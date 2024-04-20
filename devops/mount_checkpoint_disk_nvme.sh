3895e215e527b1067c4965fec884604e2a7d5e6b
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-20 15:55:35 +0000

    Send cache to NVME

--- devops/mount_checkpoint_disk_nvme.sh
+++ devops/mount_checkpoint_disk_nvme.sh
@@ -5,18 +5,21 @@ sudo mount /dev/nvme0n2 /mnt/checkpoints
 # rm ./checkpoints
 
 # ./checkpoints directory
+sudo mv ./checkpoints /mnt/checkpoints/checkpoints
 sudo mkdir /mnt/checkpoints/checkpoints
 ln -s /mnt/checkpoints/checkpoints ./checkpoints
 sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/checkpoints
 sudo chmod 766 /mnt/checkpoints/checkpoints
 
 # ./artifacts directory
+sudo mv ./artifacts /mnt/checkpoints/artifacts
 sudo mkdir /mnt/checkpoints/artifacts
 ln -s /mnt/checkpoints/artifacts ./artifacts
 sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/artifacts
 sudo chmod 766 /mnt/checkpoints/artifacts
 
 # ~/.cache directory
+sudo mv ~/.cache /mnt/checkpoints/.cache
 sudo mkdir /mnt/checkpoints/.cache
 ln -s /mnt/checkpoints/.cache ~/.cache
 sudo chown -R $(whoami):$(whoami) /mnt/checkpoints/.cache
