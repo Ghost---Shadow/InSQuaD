4a888e3479b91a0e754affc17f3c32faabd4c286
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-06 10:09:34 -0500

    Extra column in table

--- src/run_analysis_scripts/tables_in_paper.py
+++ src/run_analysis_scripts/tables_in_paper.py
@@ -43,8 +43,10 @@ GROUPS = {
 }
 
 
-def get_column_spec(groups):
+def get_column_spec(groups, extra_column_name):
     column_spec = "l"  # Start with 'l' for the 'Method' column
+    if extra_column_name:
+        column_spec += "|l"
     multicolumn_parts = []
     total_columns = sum(len(columns) for columns in groups.values())
 
@@ -66,29 +68,44 @@ def get_column_spec(groups):
             )
 
     multicolumn_line = " & ".join(multicolumn_parts)
+    if extra_column_name:
+        multicolumn_line = f"\\textbf{{{extra_column_name}}} & " + multicolumn_line
+
     multicolumn_line = "\\textbf{Method} & " + multicolumn_line
     return column_spec, multicolumn_line
 
 
-def generate_latex_rows(df, method_lut, num_columns):
+def generate_latex_rows(df, method_lut, num_columns, extra_column_lut):
     latex_rows = ""
     for method, method_print_name in method_lut.items():
         row = df[df["method"].str.startswith(method)]
+        method_column = [method_print_name]
+        extra_column = []
+        if extra_column_lut is not None:
+            extra_column = [extra_column_lut[method]]
         if len(row) == 1:
             row = row.values.tolist()[0]
-            cells = [method_print_name] + [
-                f"{x:.1f}" if pd.notna(x) else "??.?" for x in row[1:]
-            ]
+            cells = (
+                method_column
+                + extra_column
+                + [f"{x:.1f}" if pd.notna(x) else "??.?" for x in row[1:]]
+            )
         else:
-            cells = [method_print_name] + ["??.?"] * num_columns
+            cells = method_column + extra_column + ["??.?"] * num_columns
         latex_row = " & ".join(cells)
         latex_rows += latex_row + " \\\\\n"
     return latex_rows
 
 
-def generate_latex_table(df, caption, label, method_lut):
+def generate_latex_table(
+    df, caption, label, method_lut, extra_column_name=None, extra_column_lut=None
+):
     # Reset the index to make 'method' a regular column
     df = df.reset_index()
+
+    if extra_column_lut is not None:
+        assert set(extra_column_lut.keys()) == set(method_lut.keys())
+        assert extra_column_name is not None
 
     # Ensure all required columns are present in the DataFrame
     expected_columns = [col for group in GROUPS.values() for col in group]
@@ -100,8 +117,9 @@ def generate_latex_table(df, caption, label, method_lut):
     # Reorder DataFrame to match the exact column order needed
     df = df[column_order]
 
-    latex_rows = generate_latex_rows(df, method_lut, num_columns=len(expected_columns))
-    column_spec, multicolumn_line = get_column_spec(GROUPS)
+    num_columns = len(expected_columns)
+    latex_rows = generate_latex_rows(df, method_lut, num_columns, extra_column_lut)
+    column_spec, multicolumn_line = get_column_spec(GROUPS, extra_column_name)
 
     header_line = "& " + " & ".join(
         [
@@ -110,6 +128,13 @@ def generate_latex_table(df, caption, label, method_lut):
             for col in group
         ]
     )
+
+    if extra_column_name is not None:
+        header_line = "&" + header_line
+
+    offset = 0
+    if extra_column_name is not None:
+        offset = 1
 
     latex_template = f"""
 \\begin{{table}}[H]
@@ -121,7 +146,7 @@ def generate_latex_table(df, caption, label, method_lut):
 \\begin{{tabular}}{{{column_spec}}}
 \\hline
 {multicolumn_line} \\\\
-\\cmidrule(lr){{2-{len(column_order)}}}
+\\cmidrule(lr){{{2+offset}-{len(column_order)+offset}}}
 {header_line} \\\\
 \\hline
 {latex_rows}
@@ -138,13 +163,31 @@ def generate_retrieval_method_ablations(df):
     method_lut = {
         "zeroshot_mpnet_stablelm": "Zeroshot",
         "random_mpnet_stablelm": "Random",
-        "quaild_random_fl_mpnet_stablelm": "QuailD-FL (random)",
-        "quaild_random_gc_mpnet_stablelm": "QuailD-GC (random)",
-        "quaild_similar_fl_mpnet_stablelm": "QuailD-FL (similar)",
-        "quaild_similar_gc_mpnet_stablelm": "QuailD-GC (similar)",
-        "quaild_gain_fl_mpnet_stablelm": "QuailD-FL (submodular)",
-        "quaild_gain_gc_mpnet_stablelm": "QuailD-GC (submodular)",
+        "quaild_random_fl_mpnet_stablelm": "QuailD-FL",
+        "quaild_random_gc_mpnet_stablelm": "QuailD-GC",
+        "quaild_similar_fl_mpnet_stablelm": "QuailD-FL",
+        "quaild_similar_gc_mpnet_stablelm": "QuailD-GC",
+        "quaild_gain_fl_mpnet_stablelm": "QuailD-FL",
+        "quaild_gain_gc_mpnet_stablelm": "QuailD-GC",
     }
-    result = generate_latex_table(df, caption, label, method_lut)
+    extra_column_lut = {
+        "zeroshot_mpnet_stablelm": "",
+        "random_mpnet_stablelm": "",
+        "quaild_random_fl_mpnet_stablelm": "Random",
+        "quaild_random_gc_mpnet_stablelm": "Random",
+        "quaild_similar_fl_mpnet_stablelm": "Similar",
+        "quaild_similar_gc_mpnet_stablelm": "Similar",
+        "quaild_gain_fl_mpnet_stablelm": "Submodular",
+        "quaild_gain_gc_mpnet_stablelm": "Submodular",
+    }
+    extra_column_name = "Retrieval"
+    result = generate_latex_table(
+        df,
+        caption,
+        label,
+        method_lut,
+        extra_column_name,
+        extra_column_lut,
+    )
 
     return result
