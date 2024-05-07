54dbb6f6b4ed3a10674c62104fc6e9c16c0cc92a
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-06 19:25:15 -0500

    oracle strategy

---
+++ src/shortlist_strategies/oracle_strategy.py
@@ -0,0 +1,43 @@
+import json
+from config import RootConfig
+import numpy as np
+from shortlist_strategies.base import BaseStrategy
+from tqdm import tqdm
+
+
+class OracleStrategy(BaseStrategy):
+    NAME = "oracle"
+
+    def __init__(self, config: RootConfig, pipeline):
+        super().__init__(config, pipeline)
+
+    def shortlist(self, use_cache=True):
+        longlist_rows = self.subsample_dataset_for_train()
+        total = len(longlist_rows)
+
+        indexes = np.arange(total)
+        np.random.shuffle(indexes)
+        indexes = indexes[: self.config.offline_validation.annotation_budget]
+        indexes = indexes.tolist()
+        confidences = [1 / total] * len(indexes)  # Uniform
+
+        return indexes, confidences
+
+    def assemble_few_shot(self, use_cache=True):
+        with open(self.pipeline.shortlisted_data_path) as f:
+            shortlist = json.load(f)
+
+        eval_list_rows = self.subsample_dataset_for_eval()
+        for row in tqdm(eval_list_rows, desc="Assembling few shot"):
+            num_shots = self.config.offline_validation.num_shots - 1
+            num_shots = min(num_shots, len(shortlist))
+            few_shots = np.random.choice(shortlist, size=num_shots, replace=False)
+            few_shots = [*few_shots, row]  # Add the answer with n-1 distractors
+            np.random.shuffle(few_shots)
+
+            collated_few_shots = {"prompts": [], "labels": []}
+            for few_shot in few_shots:
+                collated_few_shots["prompts"].append(few_shot["prompts"])
+                collated_few_shots["labels"].append(few_shot["labels"])
+
+            yield row, collated_few_shots
---
+++ src/shortlist_strategies/oracle_strategy_test.py
@@ -0,0 +1,86 @@
+import os
+import unittest
+from config import Config
+from offline_eval_pipeline import OfflineEvaluationPipeline
+
+
+# python -m unittest shortlist_strategies.oracle_strategy_test.TestOracleStrategy -v
+class TestOracleStrategy(unittest.TestCase):
+    # python -m unittest shortlist_strategies.oracle_strategy_test.TestOracleStrategy.test_shortlist -v
+    def test_shortlist(self):
+        config = Config.from_file("experiments/tests/oracle_test_experiment.yaml")
+        pipeline = OfflineEvaluationPipeline(config)
+        pipeline.set_seed(42)
+        pipeline.current_dataset_name = "mrpc"
+        indexes, confidences = pipeline.shortlist_strategy.shortlist()
+
+        assert indexes == [
+            79,
+            233,
+            216,
+            196,
+            197,
+            66,
+            183,
+            128,
+            297,
+            163,
+            120,
+            230,
+            171,
+            267,
+            223,
+            175,
+            201,
+            57,
+            243,
+            84,
+        ], indexes
+        assert confidences == [
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+            0.0033333333333333335,
+        ], confidences
+
+    # python -m unittest shortlist_strategies.oracle_strategy_test.TestOracleStrategy.test_assemble_few_shot -v
+    def test_assemble_few_shot(self):
+        config = Config.from_file("experiments/tests/oracle_test_experiment.yaml")
+        pipeline = OfflineEvaluationPipeline(config)
+        pipeline.set_seed(42)
+        pipeline.current_dataset_name = "mrpc"
+
+        if not os.path.exists(pipeline.shortlisted_data_path):
+            pipeline.shortlist()
+
+        for row, few_shots in pipeline.shortlist_strategy.assemble_few_shot(
+            use_cache=False
+        ):
+            assert "prompts" in row, row
+            assert "labels" in row, row
+
+            assert "prompts" in few_shots, few_shots
+            assert "labels" in few_shots, few_shots
+
+            assert (
+                len(few_shots["prompts"]) == config.offline_validation.num_shots
+            ), few_shots
+            assert (
+                len(few_shots["labels"]) == config.offline_validation.num_shots
+            ), few_shots
