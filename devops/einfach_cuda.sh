def010d219dc6453bd2d3861361320662f948ad0
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-10 16:38:25 +0000

    Mark and hold

--- devops/einfach_cuda.sh
+++ devops/einfach_cuda.sh
@@ -1,3 +1,5 @@
+# sudo apt remove linux-image-5.10.0-29-cloud-amd64
+# sudo apt-mark hold linux-image-5.10.0-27-cloud-amd64
 curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
 sudo python3 install_gpu_driver.py
 sudo python3 install_gpu_driver.py verify 
