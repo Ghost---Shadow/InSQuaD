7dbccac845a39f818f25ece288b4cb2a0de21c9e
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-10 16:24:06 +0000

    Update gain analysis do everything

--- src/gain_analysis_scripts/do_everything.sh
+++ src/gain_analysis_scripts/do_everything.sh
@@ -1,7 +1,8 @@
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_gain_fl_mpnet_stablelm.yaml  --should_load_checkpoint
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_gain_gc_mpnet_stablelm.yaml --should_load_checkpoint
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_nt_fl_mpnet_stablelm.yaml   
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_nt_gc_mpnet_stablelm.yaml 
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_gain_fl_mpnet_stablelm.yaml  --should_load_checkpoint --limit=1000
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_gain_gc_mpnet_stablelm.yaml --should_load_checkpoint --limit=1000
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_nt_fl_mpnet_stablelm.yaml --limit=1000
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_nt_gc_mpnet_stablelm.yaml --limit=1000
 
 python src/gain_analysis_scripts/plot_maximum_achieveable_f1.py
 source devops/upload_artifacts.sh
+source devops/stop_current_gcp_instance.sh
