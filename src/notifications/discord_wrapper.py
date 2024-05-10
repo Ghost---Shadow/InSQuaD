de1639667bdbc0fc537ea6712b4e2258b0acdc6d
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-10 10:30:09 -0500

    Image notifications

--- src/notifications/discord_wrapper.py
+++ src/notifications/discord_wrapper.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import requests
 import json
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import argparse
 
 load_dotenv()
 
@@ -28,6 +30,44 @@ def send_discord_notification(message):
         print(e)
 
 
+def send_discord_image(image_path):
+    if os.environ.get("IS_LOCAL") == "yes":
+        # No notifications on development
+        return
+
+    try:
+        # Prepare the multipart data
+        m = MultipartEncoder(
+            fields={"file": (image_path, open(image_path, "rb"), "image/png")}
+        )
+        headers = {"Content-Type": m.content_type}
+        webhook_url = os.environ.get("DISCORD_API_HOOK")
+
+        # POST request to the webhook URL
+        response = requests.post(webhook_url, data=m, headers=headers)
+
+        # Check for success
+        if response.status_code == 200:
+            print("Image sent successfully.")
+        else:
+            print(f"Failed to send image. Status code: {response.status_code}")
+    except Exception as e:
+        print(e)
+
+
 if __name__ == "__main__":
-    message = ":eggplant: Experiment completed!"
-    send_discord_notification(message)
+    # python src/notifications/discord_wrapper.py --message="Experiments done"
+    # python src/notifications/discord_wrapper.py --image_path=artifacts/sweep_results_k.png
+    parser = argparse.ArgumentParser(description="Send notifications to Discord.")
+    parser.add_argument(
+        "--message", type=str, help="The message to send as a notification."
+    )
+    parser.add_argument("--image_path", type=str, help="The path to the image to send.")
+
+    args = parser.parse_args()
+
+    if args.message is not None:
+        send_discord_notification(args.message)
+
+    if args.image_path is not None:
+        send_discord_image(args.image_path)
