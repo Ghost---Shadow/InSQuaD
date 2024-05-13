419b0126035696c14e438ded179ebea55a5839e9
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-13 09:14:52 -0500

    all script for convenience

--- experiments/generate_all_experiments.py
+++ experiments/generate_all_experiments.py
@@ -8,41 +8,49 @@ def generate_shell_scripts(root_dir):
     UPLOAD_CHECKPOINTS = "source devops/upload_all_checkpoints.sh\n\n"
     STOP_COMMAND = "\nsource devops/stop_current_gcp_instance.sh\n"
     root_dir = os.path.abspath(root_dir)  # Ensure absolute path
-    for dirpath, dirnames, filenames in os.walk(root_dir):
-        yaml_files = [f for f in filenames if f.endswith(".yaml")]
-        if yaml_files:
-            train_script_path = os.path.join(dirpath, "_run_train.sh")
-            eval_script_path = os.path.join(dirpath, "_run_eval.sh")
+    with open("./experiments/all.sh", "w") as all_script:
+        for dirpath, dirnames, filenames in os.walk(root_dir):
+            yaml_files = [f for f in filenames if f.endswith(".yaml")]
+            if yaml_files:
+                train_script_path = os.path.join(dirpath, "_run_train.sh")
+                eval_script_path = os.path.join(dirpath, "_run_eval.sh")
 
-            with open(train_script_path, "w") as train_script, open(
-                eval_script_path, "w"
-            ) as eval_script:
-                for yaml_file in sorted(yaml_files):
-                    config_path = os.path.join(dirpath, yaml_file)
-                    # Make config_path relative to root_dir and replace backslashes if on a non-Unix system
-                    config_path = os.path.relpath(config_path, start="./").replace(
-                        "\\", "/"
-                    )
-                    with open(config_path) as f:
-                        config = yaml.safe_load(f)
-                    if config["training"]["type"] != NoOperation.NAME:
-                        train_command = f"python src/train.py --config={config_path}\n"
-                    else:
-                        train_command = None
-                    eval_command = (
-                        f"python src/offline_eval.py --config={config_path}\n"
-                    )
-                    if train_command:
-                        train_script.write(train_command)
-                        train_script.write(UPLOAD_CHECKPOINTS)
-                    eval_script.write(eval_command)
-                    eval_script.write(UPLOAD_ARTIFACTS)
-                train_script.write(STOP_COMMAND)
-                eval_script.write(STOP_COMMAND)
+                with open(train_script_path, "w") as train_script, open(
+                    eval_script_path, "w"
+                ) as eval_script:
+                    for yaml_file in sorted(yaml_files):
+                        config_path = os.path.join(dirpath, yaml_file)
+                        # Make config_path relative to root_dir and replace backslashes if on a non-Unix system
+                        config_path = os.path.relpath(config_path, start="./").replace(
+                            "\\", "/"
+                        )
+                        with open(config_path) as f:
+                            config = yaml.safe_load(f)
+                        if config["training"]["type"] != NoOperation.NAME:
+                            train_command = (
+                                f"python src/train.py --config={config_path}\n"
+                            )
+                        else:
+                            train_command = None
+                        eval_command = (
+                            f"python src/offline_eval.py --config={config_path}\n"
+                        )
+                        if train_command:
+                            train_script.write(train_command)
+                            train_script.write(UPLOAD_CHECKPOINTS)
+                        eval_script.write(eval_command)
+                        eval_script.write(UPLOAD_ARTIFACTS)
+                        if "gemma" in eval_command:
+                            all_script.write(eval_command)
+                            all_script.write(UPLOAD_ARTIFACTS)
+                    train_script.write(STOP_COMMAND)
+                    eval_script.write(STOP_COMMAND)
 
-            print(
-                f"Generated {train_script_path} and {eval_script_path} for YAML files in {dirpath}"
-            )
+                print(
+                    f"Generated {train_script_path} and {eval_script_path} for YAML files in {dirpath}"
+                )
+
+        all_script.write(STOP_COMMAND)
 
 
 if __name__ == "__main__":
