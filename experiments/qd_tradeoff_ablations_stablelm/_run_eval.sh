1f919367756b82f2eba2d2b72c27456a4ecf9473
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-12 20:05:40 -0500

    qd tradeoff ablations

---
+++ experiments/qd_tradeoff_ablations_stablelm/_run_eval.sh
@@ -0,0 +1,14 @@
+python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_stablelm/quaild_gain_fl_mpnet_stablelm_lambda_0.yaml
+source devops/upload_artifacts.sh
+
+python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_stablelm/quaild_gain_fl_mpnet_stablelm_lambda_1.yaml
+source devops/upload_artifacts.sh
+
+python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_stablelm/quaild_gain_gc_mpnet_stablelm_lambda_0.yaml
+source devops/upload_artifacts.sh
+
+python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_stablelm/quaild_gain_gc_mpnet_stablelm_lambda_1.yaml
+source devops/upload_artifacts.sh
+
+
+source devops/stop_current_gcp_instance.sh
