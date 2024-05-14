92cc7d6a04d356e27108f03cd027bca0891e9070
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-13 20:28:54 -0500

    Lambda 25 training run

--- experiments/qd_tradeoff_ablations_gemma/_run_eval.sh
+++ experiments/qd_tradeoff_ablations_gemma/_run_eval.sh
@@ -1,10 +1,16 @@
 python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_gemma/quaild_gain_fl_mpnet_gemma_lambda_0.yaml
 source devops/upload_artifacts.sh
 
+python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_gemma/quaild_gain_fl_mpnet_gemma_lambda_025.yaml
+source devops/upload_artifacts.sh
+
 python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_gemma/quaild_gain_fl_mpnet_gemma_lambda_1.yaml
 source devops/upload_artifacts.sh
 
 python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_gemma/quaild_gain_gc_mpnet_gemma_lambda_0.yaml
+source devops/upload_artifacts.sh
+
+python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_gemma/quaild_gain_gc_mpnet_gemma_lambda_025.yaml
 source devops/upload_artifacts.sh
 
 python src/offline_eval.py --config=experiments/qd_tradeoff_ablations_gemma/quaild_gain_gc_mpnet_gemma_lambda_1.yaml
