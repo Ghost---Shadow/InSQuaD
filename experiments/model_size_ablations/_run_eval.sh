0381d2f6138aed280ac75aa8344d526e84b0a32e
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-15 17:40:39 -0500

    GPTJ6B

--- experiments/model_size_ablations/_run_eval.sh
+++ experiments/model_size_ablations/_run_eval.sh
@@ -4,10 +4,16 @@ source devops/upload_artifacts.sh
 python src/offline_eval.py --config=experiments/model_size_ablations/oracle_mpnet_gemma7b.yaml
 source devops/upload_artifacts.sh
 
+python src/offline_eval.py --config=experiments/model_size_ablations/oracle_mpnet_gptj6b.yaml
+source devops/upload_artifacts.sh
+
 python src/offline_eval.py --config=experiments/model_size_ablations/quaild_gain_fl_mpnet_davinci2.yaml
 source devops/upload_artifacts.sh
 
 python src/offline_eval.py --config=experiments/model_size_ablations/quaild_gain_fl_mpnet_gemma7b.yaml
+source devops/upload_artifacts.sh
+
+python src/offline_eval.py --config=experiments/model_size_ablations/quaild_gain_fl_mpnet_gptj6b.yaml
 source devops/upload_artifacts.sh
 
 python src/offline_eval.py --config=experiments/model_size_ablations/quaild_gain_gc_mpnet_davinci2.yaml
@@ -22,10 +28,16 @@ source devops/upload_artifacts.sh
 python src/offline_eval.py --config=experiments/model_size_ablations/random_mpnet_gemma7b.yaml
 source devops/upload_artifacts.sh
 
+python src/offline_eval.py --config=experiments/model_size_ablations/random_mpnet_gptj6b.yaml
+source devops/upload_artifacts.sh
+
 python src/offline_eval.py --config=experiments/model_size_ablations/zeroshot_mpnet_davinci2.yaml
 source devops/upload_artifacts.sh
 
 python src/offline_eval.py --config=experiments/model_size_ablations/zeroshot_mpnet_gemma7b.yaml
+source devops/upload_artifacts.sh
+
+python src/offline_eval.py --config=experiments/model_size_ablations/zeroshot_mpnet_gptj6b.yaml
 source devops/upload_artifacts.sh
 
 
--- experiments/model_size_ablations/_run_train.sh
+++ experiments/model_size_ablations/_run_train.sh
@@ -4,6 +4,9 @@ source devops/upload_all_checkpoints.sh
 python src/train.py --config=experiments/model_size_ablations/quaild_gain_fl_mpnet_gemma7b.yaml
 source devops/upload_all_checkpoints.sh
 
+python src/train.py --config=experiments/model_size_ablations/quaild_gain_fl_mpnet_gptj6b.yaml
+source devops/upload_all_checkpoints.sh
+
 python src/train.py --config=experiments/model_size_ablations/quaild_gain_gc_mpnet_davinci2.yaml
 source devops/upload_all_checkpoints.sh
 
