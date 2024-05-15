41a76e67b99cb0a350e2befa101bf9f479f58bdb
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-15 15:25:09 +0000

    Fix config file paths

--- src/gain_analysis_scripts/do_everything.sh
+++ src/gain_analysis_scripts/do_everything.sh
@@ -1,7 +1,7 @@
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_gain_fl_mpnet_gemma.yaml  --should_load_checkpoint --limit=1000
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_gain_gc_mpnet_gemma.yaml --should_load_checkpoint --limit=1000
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_nt_fl_mpnet_gemma.yaml --limit=1000
-python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table/quaild_nt_gc_mpnet_gemma.yaml --limit=1000
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table_gemma/quaild_gain_fl_mpnet_gemma.yaml  --should_load_checkpoint --limit=1000
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table_gemma/quaild_gain_gc_mpnet_gemma.yaml --should_load_checkpoint --limit=1000
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table_gemma/quaild_nt_fl_mpnet_gemma.yaml --limit=1000
+python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/main_table_gemma/quaild_nt_gc_mpnet_gemma.yaml --limit=1000
 source devops/upload_artifacts.sh
 
 python src/gain_analysis_scripts/maximum_achieveable_f1.py --config=experiments/qd_tradeoff_ablations_gemma/quaild_gain_fl_mpnet_gemma_lambda_025.yaml --should_load_checkpoint --limit=1000
