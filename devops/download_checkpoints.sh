b86f534cb1c4cf67b6c6d6c3a7d94ac18adf6df4
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-05-20 23:57:35 +0000

    Example for single checkpoint

--- devops/download_checkpoints.sh
+++ devops/download_checkpoints.sh
@@ -1,1 +1,5 @@
 gsutil -m cp -n -r "gs://quaild-icl-bucket/checkpoints/*" "./checkpoints"
+
+# Single checkpoint example
+# mkdir -p "./checkpoints/050de/"
+# gsutil -m cp -n -r "gs://quaild-icl-bucket/checkpoints/050de/*" "./checkpoints/050de/"
