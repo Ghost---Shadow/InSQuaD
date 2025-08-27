db06cf0e24e98ad0191ea7e4579e43a511b406fe
Souradeep Nanda <souradeep.nanda123@gmail.com>
2025-08-27 09:53:06 +0530

    Works on my machine

--- devops/install.sh
+++ devops/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "Installing dependencies..."
+echo "Installing compatible numpy..."
+pip install "numpy>=1.21.0,<2.0.0"
+
+echo "Installing PyTorch dependencies..."
 pip install -r requirements-torch.txt --index-url https://download.pytorch.org/whl/cu121
 
+echo "Installing other dependencies..."
+pip install -r requirements.txt
+
 echo "Installing FAISS..."
-pip install faiss-cpu==1.7.4
+pip install faiss-cpu==1.12.0
 
 echo "Installing project..."
 pip install -e .
